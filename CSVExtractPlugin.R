c2832cd4acc5415627f26d1e3319028b817a564f
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-07-03 12:42:14 -0400

    Uses prefix now

--- CSVExtractPlugin.R
+++ CSVExtractPlugin.R
@@ -1,7 +1,15 @@
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+library(rlist)
+
 input <- function(inputfile) {
+  pfix = prefix()
+  if (length(pfix) != 0) {
+     prefix <- paste(pfix, "/", sep="")
+  }
   parameters <- read.table(inputfile, as.is=T);
   rownames(parameters) <- parameters[,1]
-  my_data <<- read.csv(toString(parameters["inputfile",2]), header = T)
+  my_data <<- read.csv((paste(pfix, toString(parameters["inputfile",2]), sep="")), header = T, check.names=FALSE)
   target_value <<- toString(parameters["value", 2]);
   column_number <<- as.integer(parameters["column", 2]);
 }
